786a3744c0f03d3cffaa560065df8a2d569ab4ff
Davis Shaver <davisshaver@gmail.com>
2018-01-04 15:19:30 -0500

    Don't print site icons on the admin

--- inc/class-customizer.php
+++ inc/class-customizer.php
@@ -19,6 +19,21 @@ class Customizer {
 	public function setup() {
 		add_action( 'customize_register', [ $this, 'customize_register' ] );
 		add_action( 'customize_preview_init', [ $this, 'enqueue_customize_scripts' ] );
+		add_filter( 'site_icon_meta_tags', [ $this, 'site_icon_meta_tags' ] );
+	}
+
+	/**
+	 * Don't print site icon meta tags in the admin.
+	 *
+	 * @param array $tags Tags.
+	 *
+	 * @return array Tags
+	 */
+	public function site_icon_meta_tags( $tags ) {
+		if ( is_admin() ) {
+			return array();
+		}
+		return $tags;
 	}
 
 	/**
@@ -34,10 +49,20 @@ class Customizer {
 	 * @param \WP_Customize_Manager $wp_customize Customize manager.
 	 */
 	public function customize_register( \WP_Customize_Manager $wp_customize ) {
+		/**
+		 * Opt some core fields into immediate update.
+		 */
 		$wp_customize->get_setting( 'blogname' )->transport          = 'postMessage';
 		$wp_customize->get_setting( 'header_image' )->transport      = 'postMessage';
 		$wp_customize->get_setting( 'header_image_data' )->transport = 'postMessage';
+		$wp_customize->get_setting( 'header_image_data' )->transport = 'postMessage';
 
+		/**
+		 * Remove some core fields we aren't using.
+		 */
+		$wp_customize->remove_control( 'header_textcolor' );
+		$wp_customize->remove_control( 'display_header_text' );
+		$wp_customize->remove_control( 'blogdescription' );
 	}
 }
 
