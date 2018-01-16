a2836ad49495cbf82a07077b504e727ee9061413
Davis Shaver <davisshaver@gmail.com>
2018-01-16 18:58:05 -0500

    Drop more too

--- partials/content.php
+++ partials/content.php
@@ -25,21 +25,5 @@
 				the_excerpt();
 			?>
 		</div>
-		<div class="more">
-			<div class="more-facebook">
-				<a href='http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&amp;t=<?php the_title_attribute(); ?>' class='facebookShare'>FB</a>
-				<span class="fb_like_count share-number"><?php terminal_print_facebook_count_for_post(); ?></span>
-			</div>
-			<div class="more-twitter">
-				<a href="http://twitter.com/share?url=<?php the_permalink(); ?>&amp;counturl=<?php echo esc_url( wp_get_canonical_url() ); ?>&amp;via=OnwardState&amp;text=<?php the_title_attribute(); ?>" class="twitterShare" target=_blank>T</a>
-				<span class="twitter_tweet_count share-number"><?php terminal_print_twitter_count_for_post(); ?></span>
-			</div>
-			<div class="more-comments">
-				<a href="<?php the_permalink(); ?>#comments">💬</a> <span class="share-number"><?php terminal_print_comment_count_for_post(); ?></span>
-			</div>
-			<div class="more-text">
-				<a href="<?php the_permalink(); ?>"><strong><?php esc_html_e( 'Read Post', 'terminal' ); ?></strong></a>
-			</div>
-		</div>
 	</div>
 </div>
