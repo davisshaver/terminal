b98c4754b4c786ccc16fdbcb7424b990fdcf46b1
Davis Shaver <davisshaver@gmail.com>
2018-01-24 13:38:02 -0500

    Launch first stab

---
+++ inc/widgets/class-hero-secondary-widget.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * FM Widget integration.
+ *
+ * @package Terminal
+ */
+
+if ( class_exists( '\FM_Widget' ) ) {
+
+	/**
+	 * Hero Widget.
+	 */
+	class Hero_Secondary_Widget extends \FM_Widget {
+
+		/**
+		 * Register widget with WordPress.
+		 */
+		public function __construct() {
+			parent::__construct(
+				'terminal-hero-secondary-widget',
+				__( 'Hero (Secondary)', 'terminal' )
+			);
+		}
+
+		/**
+		 * Front-end display of widget.
+		 *
+		 * @see WP_Widget::widget()
+		 *
+		 * @param array $args     Widget arguments.
+		 * @param array $instance Saved values from database.
+		 */
+		public function widget( $args, $instance ) {
+			if ( empty( $instance['category'] ) || ! is_int( $instance['category'] ) ) {
+				return;
+			}
+			$cat_query = new \WP_Query( array(
+				'posts_per_page'      => 2,
+				'ignore_sticky_posts' => true,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'terminal-placement',
+						'field'    => 'id',
+						'terms'    => $instance['category'],
+					),
+				),
+			) );
+			if ( $cat_query->have_posts() ) :
+				// phpcs:ignore
+				echo $args['before_widget'];
+				while ( $cat_query->have_posts() ) :
+					$cat_query->the_post();
+					get_template_part( 'partials/hero-secondary' );
+				endwhile;
+				// phpcs:ignore
+				echo $args['after_widget'];
+			endif;
+			wp_reset_postdata();
+		}
+
+		/**
+		 * Define the fields that should appear in the widget.
+		 *
+		 * @return array Fieldmanager fields.
+		 */
+		protected function fieldmanager_children() {
+			return [
+				'category'       => new \Fieldmanager_Select( array(
+					'first_empty' => true,
+					'datasource' => new \Fieldmanager_Datasource_Term( array(
+						'taxonomy' => 'terminal-placement',
+					) ),
+				) ),
+			];
+		}
+	}
+
+	register_widget( '\Hero_Secondary_Widget' );
+}
