8256be4da7cadf4e7c0ef00f23c10ad8f64dc306
Davis Shaver <davisshaver@gmail.com>
2018-01-26 18:24:37 -0500

    For real

--- page.php
+++ page.php
@@ -8,37 +8,24 @@
 get_header(); ?>
 <div id="container">
 	<div id="body">
-		<div id="content">
+		<div class="content">
 		<?php
 		if ( have_posts() ) :
 			while ( have_posts() ) :
 				the_post();
-		?>
-			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-				<div id="single">
-					<h1><?php the_title(); ?></h1>
-					<?php if ( has_post_thumbnail() ) : ?>
-						<div class="image">
-							<?php the_post_thumbnail( 'terminal-primary-thumbnail' ); ?>
-						</div>
-					<?php endif; ?>
-					<div class="story-text">
-						<?php the_content( '<p>Read the rest of this entry &raquo;</p>' ); ?>
-					</div>
-				</div>
-			</article>
-		<?php
+				get_template_part( 'partials/content-single', get_post_type( $post ) );
 			endwhile;
 		else :
 		?>
 			<p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'terminal' ); ?></p>
 		<?php
 		endif;
+		get_template_part( 'partials/recirc' );
 		?>
 		</div>
-		<div id="sidebar">
-			<?php get_sidebar(); ?>	
-		</div>
+		<?php
+			get_template_part( 'partials/sidebar' );
+		?>
 	</div>
 </div>
 <?php get_footer(); ?>
