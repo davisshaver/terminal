0818e5d05f0329f48df0d7f0c85df3bc64cb59f0
Davis Shaver <davisshaver@gmail.com>
2018-01-30 17:00:23 -0500

    Update

--- comments.php
+++ comments.php
@@ -20,6 +20,6 @@ if ( is_active_sidebar( 'above-comments' ) ) {
 	src="https://www.facebook.com/plugins/comments.php?href=<?php echo esc_url_raw( get_the_permalink() ); ?>"
 	scrolling="no"
 	frameborder="0"
-	style="border:none; overflow:hidden; width:100%; height:500px;"
+	style="border:none; overflow:hidden; width:100%;"
 	allowTransparency="true"
 ></iframe>
