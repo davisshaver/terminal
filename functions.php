9ac7fc2e66da7f68f29011563b67cc1a3daab1e2
Davis Shaver <davisshaver@gmail.com>
2018-02-06 07:12:41 -0500

    Cleanup author bio

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '1.0.477' );
+	define( 'TERMINAL_VERSION', '1.0.479' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
