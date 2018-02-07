55a354bcbae6e94c707f321132930896ac9fb400
Davis Shaver <davisshaver@gmail.com>
2018-02-07 08:15:09 -0500

    Hold comments for a sec

--- client/src/js/comments.js
+++ client/src/js/comments.js
@@ -1,17 +1,19 @@
 /* eslint-env browser */
 
 export function setupComments() {
-  const comments = document
-    .getElementById('facebook-comments');
-  if (comments) {
-    const height = comments
-      .contentWindow
-      .document.body.scrollHeight;
-    document.getElementById('facebook-comments').height =
-      height + 100;
-  } else {
-    console.log('No comments');
-  }
+  setTimeout(() => {
+    const comments = document
+      .getElementById('facebook-comments');
+    if (comments) {
+      const height = comments
+        .contentWindow
+        .document.body.scrollHeight;
+      document.getElementById('facebook-comments').height =
+        height + 100;
+    } else {
+      console.log('No comments');
+    }
+  }, 1000);
 }
 
 export default {
