04c30908ef925440ef64b640043385d4d174614d
Davis Shaver <davisshaver@gmail.com>
2018-02-08 09:05:40 -0500

    Drop extra character

--- partials/content-loop.php
+++ partials/content-loop.php
@@ -26,7 +26,7 @@ endif;
 	if ( has_post_thumbnail() ) :
 		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'terminal-thumbnail' );
 	?>
-		<div class="image lazyload" data-bg="<?php echo esc_url( $thumb['0'] ); ?>')">
+		<div class="image lazyload" data-bg="<?php echo esc_url( $thumb['0'] ); ?>">
 			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"></a>
 		</div>
 	<?php
