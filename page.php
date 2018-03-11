69c5b999e5975dfe8e0cc7fb912c9181b0e00d58
Davis Shaver <davisshaver@gmail.com>
2018-03-11 12:21:27 -0400

    Fix page template

--- page.php
+++ page.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Page template file
+ * Single template file
  *
  * @package Terminal
  */
 
 get_header(); ?>
+
 <div class="terminal-container">
 <?php
 if ( have_posts() ) :
@@ -18,10 +19,8 @@ else :
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'terminal' ); ?></p>
 <?php
 endif;
+get_template_part( 'partials/sidebar' );
 get_template_part( 'partials/recirc' );
 ?>
 </div>
-<?php
-	get_template_part( 'partials/sidebar' );
-?>
 <?php get_footer(); ?>
