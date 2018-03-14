0f874bf904dc7271f8613fe4b9c92d29463ba735
Davis Shaver <davisshaver@gmail.com>
2018-03-14 09:55:39 -0400

    Vbump

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '1.19' );
+	define( 'TERMINAL_VERSION', '1.20' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
