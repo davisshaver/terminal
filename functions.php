27afb2f5cc73f09b71b5490b39f8ceaf08fe47a4
Davis Shaver <davisshaver@gmail.com>
2018-03-15 07:41:08 -0400

    Update stream styles

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '1.25' );
+	define( 'TERMINAL_VERSION', '1.26' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
