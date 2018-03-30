958d95b197711a7ca75d5a9980055c9bcafb0a77
Davis Shaver <davisshaver@gmail.com>
2018-03-30 08:00:14 -0400

    Use correct widget

--- partials/footer.php
+++ partials/footer.php
@@ -15,8 +15,8 @@ $footer_data = terminal_get_footer_data( array(
 	<div class="terminal-footer-inside">
 		<div class="terminal-footer-leaderboard">
 			<?php
-			if ( is_active_sidebar( 'footer' ) ) {
-				dynamic_sidebar( 'footer' );
+			if ( is_active_sidebar( 'terminal-footer' ) ) {
+				dynamic_sidebar( 'terminal-footer' );
 			}
 			?>
 		</div>
