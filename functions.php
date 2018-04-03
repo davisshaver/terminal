652734e1572bc81dc3e9e45ee6c91ef48eeb4b17
Davis Shaver <davisshaver@gmail.com>
2018-04-03 06:51:59 -0400

    Fix footer alignment

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '2.2' );
+	define( 'TERMINAL_VERSION', '2.21' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
