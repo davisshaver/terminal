5186f00488f1e8d0087c21900f88592833f0300c
Davis Shaver <davisshaver@gmail.com>
2018-04-03 08:51:05 -0400

    No grow recirc default

--- inc/class-theme.php
+++ inc/class-theme.php
@@ -327,7 +327,7 @@ class Theme {
 			'name'          => __( 'Recirculation ', 'terminal' ),
 			'id'            => 'terminal-recirc',
 			'description'   => __( 'Recirculation', 'terminal' ),
-			'before_widget' => '<div id="%1$s" class="terminal-headline-font terminal-recirc-section terminal-card terminal-card-single %2$s">',
+			'before_widget' => '<div id="%1$s" class="terminal-headline-font terminal-recirc-section terminal-card terminal-card-single terminal-card-no-grow %2$s">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<div class="terminal-card-title terminal-card-title-breakout terminal-no-select terminal-sidebar-header-font"">',
 			'after_title'   => '</div>',
