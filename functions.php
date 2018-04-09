7fbccb92791adc9c08c3a28755355a3549850eaf
Davis Shaver <davisshaver@gmail.com>
2018-04-09 15:52:50 -0400

    Center iframe

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '2.66' );
+	define( 'TERMINAL_VERSION', '2.67' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
