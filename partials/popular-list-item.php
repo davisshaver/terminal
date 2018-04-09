2a270c03e37d640d617280c1e2b8bfd4f75c2ebc
Davis Shaver <davisshaver@gmail.com>
2018-04-09 15:25:32 -0400

    Add popular

---
+++ partials/popular-list-item.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Popular post partial.
+ *
+ * @package Terminal
+ */
+
+if ( empty( $url ) || empty( $title ) ) {
+  return;
+}
+if ( ! empty( $image_url ) ) {
+  $image_class = '';
+} else {
+  $image_url = false;
+  $image_class = 'terminal-no-photo';
+}
+
+printf(
+  '<div class="terminal-popular-list-item %s">',
+  esc_attr( $image_class )
+);
+if ( $image_url ) {
+  printf(
+    '<a href="%s" class="terminal-popular-list-item-image terminal-card-image"><img alt="%s" src="%s" /></a>',
+    esc_url( $url ),
+    esc_attr( $title ),
+    esc_url( $image_url )
+  );
+}
+?>
+  <h4 class="terminal-headline-font">
+    <?php
+      printf(
+        '<a href="%s">%s</a>',
+        esc_url( $url ),
+        $title
+      );
+    ?>
+  </h4>
+  <div class="terminal-byline terminal-text-gray-light">
+    <div class="terminal-author terminal-no-select">
+    <?php
+    printf(
+      '%s %s (%s %s)',
+      esc_html( __( 'By', 'terminal' ) ),
+      esc_html( implode( ', ', $authors ) ),
+      esc_html( number_format( $views ) ),
+      esc_html( __( 'views', 'terminal' ) )
+    );
+    ?>
+	</div>
+</div>
+</div>
--- partials/popular.php
+++ partials/popular.php
@@ -1,3 +1,49 @@
+<?php
+$api_key = getenv( 'TERMINAL_PARSELY_API_KEY' );
+$api_secret = getenv( 'TERMINAL_PARSELY_API_SECRET' );
+
+if ( empty( $api_key ) || empty( $api_secret ) ) {
+  return;
+}
+?>
+
 <div id="terminal-popular" class="terminal-popular-container terminal-hidden-no-js">
   <?php terminal_print_popular_header(); ?>
+  <div class="terminal-card terminal-card-featured terminal-card-single terminal-alignment-center">
+    <div class="terminal-card-title terminal-card-title-popular terminal-sidebar-header-font">
+      <select class="terminal-popular-select-filter" name="filter">
+        <option value="past-day"><?php esc_html_e( 'Past Day', 'terminal' ); ?></option>
+        <option value="past-week"><?php esc_html_e( 'Past Week', 'terminal' ); ?></option>
+        <option value="past-month"><?php esc_html_e( 'Past Month', 'terminal' ); ?></option>
+      </select>
+    </div>
+    <?php
+    $result = wp_cache_get( 'terminal-parsely-popular-last-day' );
+    if ( false === $result ) {
+      $result = wp_remote_get(
+        "https://api.parsely.com/v2/analytics/posts?apikey=${api_key}&secret=${api_secret}&period_start=24h&limit=12"
+      );
+      wp_cache_set( 'terminal-parsely-popular-last-day', $result, '', 3600 );
+    }
+    if ( ! empty( $result ) ) {
+      $json = json_decode( wp_remote_retrieve_body( $result ) );
+      if ( ! empty( $json->data ) ) {
+        echo '<div data-value="past-day" class="terminal-card-text terminal-popular-list terminal-popular-list-day">';
+        foreach( $json->data as $popular_post ) {
+          terminal_print_template_part(
+            'popular-list-item',
+            array(
+              'url' => $popular_post->url,
+              'image_url' => $popular_post->image_url,
+              'authors' => $popular_post->authors,
+              'views' => $popular_post->metrics->views,
+              'title' => $popular_post->title,  
+            )
+          );
+        }
+        echo '</div>';
+      }
+    }
+    ?>
+  </div>
 </div>
