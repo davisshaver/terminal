9f3402e58bb85c756316bf32c30b8b689679fb80
Davis Shaver <davisshaver@gmail.com>
2018-04-13 07:19:01 -0400

    Inc caching

--- partials/social-counts.php
+++ partials/social-counts.php
@@ -15,10 +15,19 @@ $url = get_the_permalink();
 
 $result = wp_cache_get( 'terminal-social-' . $id );
 if ( false === $result ) {
+  $time_since_publish = current_time( 'timestamp' ) - get_the_time( 'U' );
+  // If published more than a week ago, cache longer.
+  if ( $time_since_publish > 6048000) {
+    $cache_length = 864000;
+  } elseif ( $time_since_publish > 604800) {
+    $cache_length = 86400;
+  } else {
+    $cache_length = 8640;
+  }
   $result = wp_remote_get(
     "https://api.parsely.com/v2/shares/post/detail?apikey=${api_key}&secret=${api_secret}&url=${url}"
   );
-  wp_cache_set( 'terminal-social-' . $id, $result, '', 3600 );
+  wp_cache_set( 'terminal-social-' . $id, $result, '', $cache_length );
 }
 
 if ( empty( $result ) ) {
