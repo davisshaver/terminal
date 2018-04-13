7a8c5073a6d05231e82e62082d4a1383fafff9eb
Davis Shaver <davisshaver@gmail.com>
2018-04-12 20:06:42 -0400

    Update

--- index.php
+++ index.php
@@ -13,23 +13,25 @@
  */
 
 get_header();
-echo '<div class="terminal-filter terminal-link-gray terminal-scroll-show terminal-viewing-popular-active">';
-echo '<div class="terminal-filter-inside">';
-printf(
-	'<a href="#featured" class="terminal-viewing-featured-active">%s</a>',
-	esc_html( __( 'Featured', 'terminal' ) )
-);
-printf(
-	'<a href="#popular" class="terminal-popular-link terminal-viewing-popular-active">%s</a>',
-	esc_html( __( 'Trending', 'terminal' ) )
-);
-printf(
-	'<a href="#recent" class="terminal-viewing-content-active">%s</a>',
-	esc_html( __( 'Recent', 'terminal' ) )
-);
-echo '</div>';
-echo '</div>';
-echo '<a id="terminal-featured" name="featured"></a>';
+if ( is_home() && ! is_paged() ) {
+	echo '<div class="terminal-filter terminal-link-gray terminal-scroll-show terminal-viewing-popular-active">';
+	echo '<div class="terminal-filter-inside">';
+	printf(
+		'<a href="#featured" class="terminal-viewing-featured-active">%s</a>',
+		esc_html( __( 'Featured', 'terminal' ) )
+	);
+	printf(
+		'<a href="#popular" class="terminal-popular-link terminal-viewing-popular-active">%s</a>',
+		esc_html( __( 'Trending', 'terminal' ) )
+	);
+	printf(
+		'<a href="#recent" class="terminal-viewing-content-active">%s</a>',
+		esc_html( __( 'Recent', 'terminal' ) )
+	);
+	echo '</div>';
+	echo '</div>';
+	echo '<a id="terminal-featured" name="featured"></a>';
+}
 ?>
 <div id="terminal-container" class="terminal-container">
 	<?php
