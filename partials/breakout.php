bd1e2a97a1fd2e2c7af438161de6560d7bb43af7
Davis Shaver <davisshaver@gmail.com>
2018-04-16 15:20:34 -0400

    Add right arrow

--- partials/breakout.php
+++ partials/breakout.php
@@ -9,5 +9,7 @@ if ( ! is_active_sidebar( 'terminal-breakout' ) ) {
 	return;
 }
 echo '<div class="terminal-breakout-container">';
+echo '<div class="terminal-breakout">';
 dynamic_sidebar( 'terminal-breakout' );
+echo '</div>';
 echo '</div>';
