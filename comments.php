2efdb3244241c5f804ad6695aa868fa7e1563dab
Davis Shaver <davisshaver@gmail.com>
2018-04-23 18:33:55 -0400

    Limit max width

--- comments.php
+++ comments.php
@@ -12,6 +12,8 @@ if ( is_active_sidebar( 'terminal-above-comments' ) ) {
 
 if ( function_exists( 'coral_talk_comments_template' ) ) {
 	terminal_print_comments_header();
+	echo '<div class="terminal-limit-max-content-width-add-margin">';
 	coral_talk_comments_template();
+	echo '</div>';
 }
 echo '</div>';
