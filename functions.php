1b8c5c92b28435bba471185073ab59fb95f62cda
Davis Shaver <davisshaver@gmail.com>
2018-04-27 07:25:34 -0400

    update

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '3.28' );
+	define( 'TERMINAL_VERSION', '3.29' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
