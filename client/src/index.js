91a092a2d32a890d4aba8033d2ff3fb28ef73ba4
Davis Shaver <davisshaver@gmail.com>
2018-04-28 12:27:42 -0400

    delay

--- client/src/index.js
+++ client/src/index.js
@@ -143,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
   exponentialBackoff(
     () => (window.pbjs) || (window.googletag && window.googletag.pubadsReady),
     3,
-    13,
+    14,
     coveredUncovered,
     coveredUncovered,
   );
