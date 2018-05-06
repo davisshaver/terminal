c3fed6ad296b397990385fa40cf4f950c45af3e3
Davis Shaver <davisshaver@gmail.com>
2018-05-06 05:55:15 -0400

    Adjust analytics

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ if ( version_compare( strtok( $GLOBALS['wp_version'], '-' ), '4.9', '<' ) ) {
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
-	define( 'TERMINAL_VERSION', '3.63' );
+	define( 'TERMINAL_VERSION', '3.64' );
 }
 
 if ( ! defined( 'TERMINAL_APP' ) ) {
