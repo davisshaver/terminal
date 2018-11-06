e7319644dbb238f5a77151152e16a4c13c4d51db
Davis Shaver <davisshaver@gmail.com>
2018-11-05 19:46:36 -0500

    list item should be link

--- partials/list-item.php
+++ partials/list-item.php
@@ -14,9 +14,17 @@ $post_thumbnail_id = get_post_thumbnail_id( $post );
 echo '<div class="terminal-list-item">';
 	if ( has_post_thumbnail() ) : ?>
 		<figure class="entry__thumbnail">
-			<?php the_post_thumbnail( 'ampnews-1040x585', array(
-				'data-amp-layout' => 'intrinsic',
-			) ); ?>
+			<?php
+			printf(
+				'<a href="%s">%s</a>',
+				esc_url( get_the_permalink() ),
+				esc_attr( get_the_title() )
+			);
+			?>
+				<?php the_post_thumbnail( 'ampnews-1040x585', array(
+					'data-amp-layout' => 'intrinsic',
+				) ); ?>
+			</a>
 		</figure>
 	<?php endif; ?>
 	<h5 class="entry__title entry__title-sidebar">
