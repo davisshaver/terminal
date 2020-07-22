887991ee5936f31e0fa6354edaeec721066f307a
Davis Shaver <davisshaver@gmail.com>
2020-07-22 14:50:40 -0400

    Drop for newspack

--- inc/class-newspack.php
+++ inc/class-newspack.php
@@ -18,9 +18,37 @@ class Newspack {
      * Setup actions.
      */
     public function setup() {
+        register_sidebar(
+            [
+                'name'          => __( 'After header', 'terminal' ),
+                'id'            => 'after-header',
+                'description'   => __( 'Add widgets here to appear after header.', 'terminal' ),
+                'before_widget' => '<section id="%1$s" class="after-header widget %2$s">',
+                'after_widget'  => '</section>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+            ]
+        );
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ], 10 );
+        add_action( 'after_header', [ $this, 'do_after_header' ] );
+    }
+
+    /**
+     * Enqueue styles.
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style( TERMINAL_APP, plugin_dir_url( dirname( __FILE__ ) ) . '/client/build/index.css', array(), TERMINAL_VERSION );
+        wp_enqueue_script( 'terminal-broadstreet', 'https://static.lebtown.com/init-2.min.js' );
+        wp_add_inline_script( 'terminal-broadstreet', "broadstreet.watch({ domain: 'sponsors.lebtown.com' })" );
 
     }
 
+    /**
+     * Do after header actions.
+     */
+    public function do_after_header() {
+        dynamic_sidebar( 'after-header' );
+    }
 }
 
 add_action( 'after_setup_theme', [ '\Terminal\Newspack', 'instance' ] );
--- terminal.php
+++ terminal.php
@@ -12,76 +12,22 @@ Text Domain: terminal
 
 namespace Terminal;
 
-/**
- * Set root path.
- */
-$root_path = realpath( __DIR__ );
-
-/**
- * Include the Composer autoload
- */
-if (
-	file_exists( $root_path . '/vendor/autoload.php' )
-) {
-	require_once $root_path . '/vendor/autoload.php';
+if ( ! defined( 'TERMINAL_APP' ) ) {
+    define( 'TERMINAL_APP', 'terminal-app' );
 }
 
 if ( ! defined( 'TERMINAL_VERSION' ) ) {
 	define( 'TERMINAL_VERSION', '4.45' );
 }
 
-if ( ! defined( 'TERMINAL_APP' ) ) {
-	define( 'TERMINAL_APP', 'terminal-app' );
-}
-
-if ( getenv( 'TERMINAL_DISABLE_WP_CRON' ) && ! defined( 'DISABLE_WP_CRON' ) ) {
-	define( 'DISABLE_WP_CRON', true );
-}
-
-if ( getenv( 'TERMINAL_WP_CRON_LOCK_TIMEOUT' ) && ! defined( 'TERMINAL_WP_CRON_LOCK_TIMEOUT' ) ) {
-	define( 'WP_CRON_LOCK_TIMEOUT', getenv( 'TERMINAL_WP_CRON_LOCK_TIMEOUT' ) );
-}
-
 require_once __DIR__ . '/lib/singleton.php';
 
-require_once __DIR__ . '/inc/class-amp.php';
-require_once __DIR__ . '/inc/class-customizer.php';
-require_once __DIR__ . '/inc/class-data.php';
-require_once __DIR__ . '/inc/class-frontend.php';
-require_once __DIR__ . '/inc/class-memberpress.php';
-require_once __DIR__ . '/inc/class-metaboxes.php';
 require_once __DIR__ . '/inc/class-newspack.php';
-require_once __DIR__ . '/inc/class-parsely.php';
-require_once __DIR__ . '/inc/class-contact.php';
-require_once __DIR__ . '/inc/class-theme.php';
-require_once __DIR__ . '/inc/class-weather.php';
 require_once __DIR__ . '/inc/class-widgets.php';
-require_once __DIR__ . '/inc/data/class-ad-data.php';
-require_once __DIR__ . '/inc/data/class-apps-data.php';
 require_once __DIR__ . '/inc/template-tags.php';
-
-add_filter( 'mepr-validate-signup', function( $errors ) {
-	return Memberpress::instance()->check_email_domain( $errors );
-} );
-
-add_filter( 'mepr-validate-account', function( $errors ) {
-	return Memberpress::instance()->check_email_domain( $errors );
-} );
 
 if ( getenv( 'TERMINAL_ENABLE_LINK_POST_TYPE' ) ) {
 	require_once __DIR__ . '/inc/post-types/class-links.php';
-}
-
-if ( getenv( 'TERMINAL_ENABLE_BOOK_POST_TYPE' ) ) {
-	require_once __DIR__ . '/inc/post-types/class-books.php';
-}
-
-if ( getenv( 'TERMINAL_ENABLE_COMMUNITY_POST_TYPE' ) ) {
-	require_once __DIR__ . '/inc/post-types/class-community.php';
-}
-
-if ( getenv( 'TERMINAL_ENABLE_HOUSING_POST_TYPE' ) ) {
-	require_once __DIR__ . '/inc/post-types/class-housing.php';
 }
 
 if ( getenv( 'TERMINAL_ENABLE_OBITUARY_POST_TYPE' ) ) {
@@ -90,12 +36,4 @@ if ( getenv( 'TERMINAL_ENABLE_OBITUARY_POST_TYPE' ) ) {
 
 if ( getenv( 'TERMINAL_ENABLE_RELEASES_POST_TYPE' ) ) {
 	require_once __DIR__ . '/inc/post-types/class-releases.php';
-}
-
-if ( getenv( 'TERMINAL_ENABLE_DEALS_POST_TYPE' ) ) {
-	require_once __DIR__ . '/inc/post-types/class-deals.php';
-}
-
-if ( getenv( 'TERMINAL_ENABLE_JOBS_POST_TYPE' ) ) {
-	require_once __DIR__ . '/inc/post-types/class-jobs.php';
 }
